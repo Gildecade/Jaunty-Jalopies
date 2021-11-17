0b784bcc0a507725771dbc3b859465560d0c98b6
Xinwei Qin <xqin49@gatech.edu>
2021-11-17 16:13:11 +0800

    edit api module

--- demo/authentication/login.js
+++ demo/authentication/login.js
@@ -8,7 +8,7 @@ const database = require('scf-nodejs-serverlessdb-sdk').database;
  * Input: username and password
  * Ouput: usertype or null(failed)
  */
-router.post('/login', async (req, res) => {
+router.post('/', async (req, res) => {
     // Check if user record matches
     const{username, password} = req.body;
     let sql = ` SELECT username
@@ -17,47 +17,46 @@ router.post('/login', async (req, res) => {
     try {
         const pool = await database('TEST').pool();
         let username = await pool.queryAsync(sql);
-        if (username == null) {
+        if (username.length == 0) {
           res.send(null);
         }
         // Find user type
         sql = ` SELECT username
                 FROM Owner
-                WHERE username = '${username}''`;
-        exist = await pool.queryAsync(sql);
-        if(exist !=null){
+                WHERE username = '${username}'`;
+        let exist1 = await pool.queryAsync(sql);
+        if(exist1.length != 0){
             res.send("Owner");
         }
-
         sql = ` SELECT username
                 FROM InventoryClerk
-                WHERE username = '${username}''`;
-        exist = await pool.queryAsync(sql);
-        if(exist !=null){
+                WHERE username = '${username}'`;
+        let exist2 = await pool.queryAsync(sql);
+        if(exist2.length != 0){
             res.send("Inventory Clerk");
         }
 
         sql = ` SELECT username
                 FROM Salespeople
-                WHERE username = '${username}''`;
-        exist = await pool.queryAsync(sql);
-        if(exist !=null){
+                WHERE username = '${username}'`;
+        let exist3 = await pool.queryAsync(sql);
+        if(exist3.length != 0){
             res.send("Salespeople");
         }
 
         sql = ` SELECT username
                 FROM ServiceWriter
-                WHERE username = '${username}''`;
-        exist = await pool.queryAsync(sql);
-        if(exist !=null){
+                WHERE username = '${username}'`;
+        let exist4 = await pool.queryAsync(sql);
+        if(exist4.length != 0){
         res.send("Service Writer");
         }
 
         sql = ` SELECT username
                 FROM Manager
-                WHERE username = '${username}''`;
-        exist = await pool.queryAsync(sql);
-        if(exist !=null){
+                WHERE username = '${username}'`;
+        let exist5 = await pool.queryAsync(sql);
+        if(exist5.length != 0){
         res.send("Manager");
         }
       } catch (err) {
@@ -65,3 +64,5 @@ router.post('/login', async (req, res) => {
         res.status(500).send({ error: err });
       }
 });
+
+module.exports = router;
