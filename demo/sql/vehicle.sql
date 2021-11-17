f31ca4cf88e7d1995cde85b91cd38bb681222ae6
Xinwei Qin <xqin49@gatech.edu>
2021-11-17 21:18:28 +0800

    add vehicle related sqls

--- demo/sql/vehicle.sql
+++ demo/sql/vehicle.sql
@@ -1,5 +1,89 @@
-INSERT INTO Vehicle
-VALUES(1, 100, 2020, "model_name_1", "aaa", "2020-1-1", "InventoryClerk1", "manfacturer_1", "car");
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(1, 100, 2018, "model_name_1", "aaa", "2018-1-2", "InventoryClerk1", "Acura", "car");
 
-INSERT INTO Vehicle
-VALUES(2, 200, 2019, "model_name_2", "bbb", "2020-1-11", "InventoryClerk2", "manfacturer_2", "convertible");
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(2, 200, 2018, "model_name_1", "bbb", "2018-1-20", "InventoryClerk2", "Acura", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(3, 300, 2018, "model_name_2", "sss", "2018-1-21", "InventoryClerk1", "Acura", "convertible");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(4, 200, 2018, "model_name_1", "bbb", "2018-1-23", "InventoryClerk1", "Acura", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(5, 200, 2018, "model_name_1", "bbb", "2018-1-24", "InventoryClerk1", "Acura", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(6, 200, 2018, "model_name_1", "bbb", "2018-1-24", "InventoryClerk1", "Acura", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(7, 200, 2018, "model_name_1", "bbb", "2018-1-24", "InventoryClerk1", "Acura", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(8, 300, 2018, "model_name_2", "sss", "2018-2-1", "InventoryClerk1", "Acura", "convertible");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(9, 300, 2018, "model_name_2", "sss", "2018-2-1", "InventoryClerk3", "Acura", "convertible");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(10, 300, 2018, "model_name_2", "sss", "2018-2-1", "InventoryClerk3", "Acura", "convertible");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(11, 500, 2018, "model_name_3", "sss", "2018-3-1", "InventoryClerk3", "BMW", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(12, 500, 2018, "model_name_3", "sss", "2018-3-1", "InventoryClerk2", "BMW", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(13, 500, 2018, "model_name_3", "sss", "2018-3-1", "InventoryClerk2", "BMW", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(14, 500, 2018, "model_name_3", "sss", "2018-3-1", "InventoryClerk2", "BMW", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(15, 600, 2018, "model_name_4", "sss", "2018-3-1", "InventoryClerk1", "BMW", "truck");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(16, 500, 2018, "model_name_4", "sss", "2018-3-1", "InventoryClerk1", "BMW", "truck");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(17, 500, 2018, "model_name_4", "sss", "2018-3-1", "InventoryClerk2", "BMW", "truck");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(18, 600, 2019, "model_name_5", "ssss", "2019-3-1", "InventoryClerk4", "Dodge", "vanMinivan");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(19, 600, 2019, "model_name_5", "ssss", "2019-3-12", "InventoryClerk2", "Dodge", "vanMinivan");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(20, 600, 2019, "model_name_5", "ssss", "2019-3-12", "InventoryClerk2", "Dodge", "vanMinivan");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(22, 600, 2019, "model_name_5", "ssss", "2019-3-13", "InventoryClerk4", "Dodge", "vanMinivan");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(23, 600, 2019, "model_name_5", "ssss", "2019-3-14", "InventoryClerk1", "Dodge", "vanMinivan");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(24, 1000, 2019, "model_name_6", "ssss", "2019-5-14", "InventoryClerk2", "Jeep", "suv");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(25, 800, 2019, "model_name_7", "sssss", "2019-5-20", "InventoryClerk1", "Kia", "suv");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(26, 400, 2019, "model_name_8", "ssss", "2019-5-23", "InventoryClerk3", "Nissan", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(27, 400, 2019, "model_name_8", "ssss", "2019-5-24", "InventoryClerk1", "Nissan", "car");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(28, 300, 2019, "model_name_9", "sdsa", "2019-5-25", "InventoryClerk4", "Nissan", "convertible");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(29, 1500, 2019, "model_name_10", "sdsa", "2019-5-28", "InventoryClerk2", "Porsche", "vanMinivan");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(30, 1200, 2019, "model_name_11", "sdsa", "2019-5-30", "InventoryClerk1", "Porsche", "truck");
+
+INSERT INTO Vehicle(vin, invoice_price, model_year, model_name, description, added_date, inventory_clerk_username, manufacturer, vehicle_type)
+VALUES(31, 500, 2019, "model_name_12", "sdsass", "2019-6-21", "InventoryClerk3", "Lexus", "truck");
--- demo/sql/vehicleColor.sql
+++ demo/sql/vehicleColor.sql
@@ -1,9 +1,107 @@
 INSERT INTO VehicleColor
-VALUES(1, "red");
+VALUES(1, "Red");
 
 INSERT INTO VehicleColor
-VALUES(2, "blue");
+VALUES(2, "Blue");
 
 INSERT INTO VehicleColor
-VALUES(2, "green");
+VALUES(2, "Green");
 
+INSERT INTO VehicleColor
+VALUES(3, "Copper");
+
+INSERT INTO VehicleColor
+VALUES(4, "Cream");
+
+INSERT INTO VehicleColor
+VALUES(5, "Blue");
+
+INSERT INTO VehicleColor
+VALUES(6, "Black");
+
+INSERT INTO VehicleColor
+VALUES(7, "Black");
+
+INSERT INTO VehicleColor
+VALUES(8, "Purple");
+
+INSERT INTO VehicleColor
+VALUES(8, "White");
+
+INSERT INTO VehicleColor
+VALUES(8, "Rose");
+
+INSERT INTO VehicleColor
+VALUES(9, "Gold");
+
+INSERT INTO VehicleColor
+VALUES(10, "Gold");
+
+INSERT INTO VehicleColor
+VALUES(11, "Black");
+
+INSERT INTO VehicleColor
+VALUES(12, "Beige");
+
+INSERT INTO VehicleColor
+VALUES(13, "Gray");
+
+INSERT INTO VehicleColor
+VALUES(14, "Maroon");
+
+INSERT INTO VehicleColor
+VALUES(15, "Orange");
+
+INSERT INTO VehicleColor
+VALUES(16, "Navy");
+
+INSERT INTO VehicleColor
+VALUES(17, "Green");
+
+INSERT INTO VehicleColor
+VALUES(18, "Metallic");
+
+INSERT INTO VehicleColor
+VALUES(19, "Pink");
+
+INSERT INTO VehicleColor
+VALUES(19, "Rust");
+
+INSERT INTO VehicleColor
+VALUES(20, "Navy");
+
+INSERT INTO VehicleColor
+VALUES(22, "Silver");
+
+INSERT INTO VehicleColor
+VALUES(23, "Beige");
+
+INSERT INTO VehicleColor
+VALUES(24, "White");
+
+INSERT INTO VehicleColor
+VALUES(25, "Black");
+
+INSERT INTO VehicleColor
+VALUES(26, "Red");
+
+INSERT INTO VehicleColor
+VALUES(27, "Red");
+
+INSERT INTO VehicleColor
+VALUES(28, "Red");
+
+INSERT INTO VehicleColor
+VALUES(28, "Pink");
+
+INSERT INTO VehicleColor
+VALUES(29, "Red");
+
+INSERT INTO VehicleColor
+VALUES(30, "Red");
+
+INSERT INTO VehicleColor
+VALUES(30, "White");
+
+INSERT INTO VehicleColor
+VALUES(31, "Red");
