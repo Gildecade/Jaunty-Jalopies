436e2957d55292af461ca3d6eb043b93360cd0a9
Xinwei Qin <xqin49@gatech.edu>
2021-11-25 14:29:58 +0800

    edit averaget time based on piazza

--- demo/viewReport/viewReport.js
+++ demo/viewReport/viewReport.js
@@ -259,7 +259,7 @@ router.post("/belowCostSale", async(req, res) => {
 // view average time in inventory
 router.post("/averageTime", async(req, res) => {
     let sql = `SELECT t1.vehicle_type as vehicle_type,
-    avg(datediff(t2.purchase_date, t1.added_date)) as average_time
+    avg(datediff(t2.purchase_date, t1.added_date))+1 as average_time
     FROM Vehicle t1
     INNER JOIN Sale t2
     ON t1.vin = t2.vin
